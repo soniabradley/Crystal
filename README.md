e695091fc22e2d7a6b968da260b5470374db9d6e
soniab <sonia.bradley524@gmail.com>
2017-12-17 20:42:45 -0600

    game works 100%

--- README.md
+++ README.md
@@ -3,13 +3,41 @@ Week 4 Assignment
 
 Overview
 
-This another fun and interactive game for web browsers. This time, the app dynamically updates the HTML pages with the jQuery library.
+This is another fun and interactive game for web browsers. This time, the app dynamically updates the HTML pages with the jQuery library.
 
-Inside the week-4-game folder, you will find index.html file and a  folder called assets.
+Inside the week-4-game folder, Crystal Collector, you will find index.html file and a  folder called assets.
 
 Inside the assets directory, there are three additional folders: javascript, css and images.
-Inside the javascript folder is a file called game.js.
 
-In the css folder are two files, style.css and reset.css., code found from the Meyerweb Reset. 
+Inside the javascript folder is a file called game.js.  This folder contains all the javascript language.  
 
-Images folder includes the file with the 4 crystal images I used.
+In the css folder are two files, style.css and reset.css.  The reset.css file contains code found from the Meyerweb Reset. 
+
+Images folder includes the file with the 4 crystal images.
+
+# CONCEPTS & HIGHLIGHTS:
+
+Script tag lists parent folder, assets/javascript/game.js
+
+<script type="text/javascript" src="assets/javascript/game.js"></script>
+
+Math.random generates a number between 0 and 1, that isn't a whole number, and also isn't 1. To get a number, for example between 0 and 10, multiply your answer by 10:
+Math.random() * 10
+
+To get it to be a whole number, i.e. an integer, apply Math.floor, which rounds down to the nearest whole number:
+Math.floor(Math.random() * 10)
+To get a whole number between 1 and 10, add 1 to the answer:
+Math.floor(Math.random() * 10 + 1)
+
+// Set a new Target Score (between 19 and 120)
+    targetScore = Math.floor(Math.random() * (max - min + 1)) + min;
+created instead:
+var getRandom = function(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+USE jQuery i.e.
+$("#test").hide()
+Demonstrates the jQuery hide() method, hiding the element with id="test".
+
+Change the HTML to reflect changes in currentScore
+$("#yourScore").html(currentScore);
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,122 @@
-$(document).ready(function(){
+// Global Variables
+// __________________________________________
+// Crystal variables and Value
+var crystal = {
+    blue: {
+        name: "Blue",
+        value: 0
+    },
+    green: {
+        name: "Green",
+        value: 0
+    },
+    red: {
+        name: "Red",
+        value: 0
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0
+    }
+}
+// Scores (Current anb Target)
+var currentScore = 0;
+var targetScore = 0;
 
+// Wins and Losses
+var winCount = 0;
+var lossCount = 0;
 
-var randomRed = Math.floor(Math.random()*20);
+// Functions
+// ____________________________________________
 
+var getRandom = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-$("#red").on("click", function(){
+var startGame = function () {
 
-$(".addition").html( "randomRed"); 
+    // Reset the Current Score
+    currentScore = 0;
 
+    // Set a new Target Score (between 19 and 120)
+    targetScore = getRandom(19, 120);
 
+    // Set different value for each of the crystals (between 1 and 12)
+    // Path
+    crystal.blue.value = getRandom(1, 12);
+    crystal.red.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
 
+    // Change the HTML to reflect all of these changes
+    $("#yourScore").html(currentScore);
+    $("#targetScore").html(targetScore);
+    // Testing Console
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value + " | Yellow: " + crystal.yellow.value);
+}
 
+// Respond to clicks on the crystals
+var addValues = function(crystal) {
+    currentScore = currentScore + crystal.value;
 
-var randomBlue = Math.floor(Math.random()*15);
+    // Change the HTML to reflect changes in currentScore
+    $("#yourScore").html(currentScore);
 
+    // Call the checkWin Function
+    checkWin();
 
-$("#blue").on("click", function(){
+    // Testing Console
+    console.log("Your Score:" + currentScore);
+}
 
-$(".addition").html( "randomBlue"); 
+// Check if User Won or Lost and Reset the Game
+var checkWin = function() {
+    // Check if currentScore is larger than targetScore
+    if(currentScore > targetScore) {
+        alert("Sorry. You Lost");
+        console.log("You Lost");
 
+        // add to Loss Counter
+        lossCount++;
 
+        // Change Loss Count HTML
+        $("#lossCount").html(lossCount);
 
-var randomYellow = Math.floor(Math.random()*15);
+        // ***Restart the game***
+        startGame();
+    }
+    else if(currentScore == targetScore) {
+        alert("Hurray! You Won");
+        console.log("You Won");
 
+        // add to Win Counter
+        winCount++;
 
-$("#yellow").on("click", function(){
+        // Change Win Count HTML
+        $("#winCount").html(winCount);
 
-$(".addition").html( "randomYellow"); 
+        // ***Restart the game***
+        startGame();
+    }
+}
 
+// Main Process
+// ______________________________________________
 
-var randomGreen = Math.floor(Math.random()*15);
+// Starts the game the first time.
+startGame();
 
-
-$("#green").on("click", function(){
-
-$(".addition").html( "randomGreen"); 
-
+$("#blue").click(function(){
+    addValues(crystal.blue);
+});
+$("#green").click(function(){
+    addValues(crystal.green);
+});
+$("#red").click(function(){
+    addValues(crystal.red);
+});
+$("#yellow").click(function(){
+    addValues(crystal.yellow);
 });
